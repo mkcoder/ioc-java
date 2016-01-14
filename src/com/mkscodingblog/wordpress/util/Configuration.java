8e9111186b82a0844acf75159b844de683de3efe
Muhammad K Khan <mk.muhammad.khan10@gmail.com>
2016-01-14 11:52:59 -0600

    init commit
    first commit…

---
+++ src/com/mkscodingblog/wordpress/util/Configuration.java
@@ -0,0 +1,18 @@
+package com.mkscodingblog.wordpress.util;
+
+import java.io.File;
+
+public class Configuration {
+
+	/**
+	 * 
+	 */
+	public Configuration(String filename) {		
+		File file = new File(filename);
+		if ( file.exists() )
+		{
+			
+		}
+	}
+	
+}
